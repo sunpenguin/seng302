05d216f8d8c01786883c8825cd1a70990e57a2bb
Callum Slaven <csl62@uclive.ac.nz>
2017-03-02 10:51:11 +1300

    speeling errors

--- src/main/java/seng302/App.java
+++ src/main/java/seng302/App.java
@@ -4,6 +4,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Welcttome to SENG302" );
+        System.out.println( "Welcome to SENG302" );
     }
 }
--- src/test/java/seng302/AppTest.java
+++ src/test/java/seng302/AppTest.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 import static org.junit.Assert.assertTrue;
 
 /**
- * hi
+ * HI
  */
 public class AppTest 
 {
