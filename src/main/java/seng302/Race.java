30b59bd96f9d151c17d404000e03be84e65fdb90
Kiwizftw <justinthorby@yahoo.co.nz>
2017-03-04 11:37:27 +1300

    Added suitable javadoc comments to the Race class.

--- src/main/java/seng302/Race.java
+++ src/main/java/seng302/Race.java
@@ -12,27 +12,58 @@ class Race {
     private ArrayList<Boat> startingList = new ArrayList<>();
     private Course course;
 
+
+    /**
+     * Race class constructor.
+     * @param startingList Arraylist holding all entered boats
+     * @param course Course object
+     */
     public Race(ArrayList<Boat> startingList, Course course) {
         this.startingList = startingList;
         this.course = course;
     }
 
-    public void setStartingList(ArrayList<Boat> startingList) {
-        this.startingList = startingList;
-    }
 
-    public Course getCourse() {
-        return course;
-    }
-
-    public void setCourse(Course course) {
-        this.course = course;
-    }
-
+    /**
+     * Starting list getter.
+     * @return Arraylist holding all entered boats
+     */
     ArrayList<Boat> getStartingList() {
         return startingList;
     }
 
+
+    /**
+     * Starting list setter.
+     * @param startingList Arraylist holding all entered boats
+     */
+    public void setStartingList(ArrayList<Boat> startingList) {
+        this.startingList = startingList;
+    }
+
+
+    /**
+     * Course getter.
+     * @return Course object
+     */
+    public Course getCourse() {
+        return course;
+    }
+
+
+    /**
+     * Course setter.
+     * @param course Course object
+     */
+    public void setCourse(Course course) {
+        this.course = course;
+    }
+
+
+    /**
+     * Add a new Boat to the starting list of boats.
+     * @param boatToAdd Boat object
+     */
     void addBoat(Boat boatToAdd) {
         boolean nameDifferent = true;
 
@@ -48,10 +79,18 @@ class Race {
         }
     }
 
+
+    /**
+     * Shuffles the starting list of boats.
+     */
     void randomiseOrder(){
         Collections.shuffle(this.startingList);
     }
 
+
+    /**
+     * Displays team and boat names of each boat in the starting list..
+     */
     void viewBoats() {
         for (Boat boat : startingList) {
             System.out.printf("%s %s\n", boat.getTeamName(), boat.getBoatName());
@@ -59,7 +98,7 @@ class Race {
     }
 
     /**
-     * Displays the finishing order of the race to the user
+     * Displays the finishing order of the race to the user.
      */
     void viewFinishOrder() {
         for (int i = 0; i < startingList.size(); i++){
@@ -67,6 +106,10 @@ class Race {
         }
     }
 
+
+    /**
+     * Display each boat in the race and their position at each mark.
+     */
     void runRace(){
         System.out.println("Start");
         for(int i = 1; i < course.marks.size(); i++){
