143b65ad63b9f4083f863aa05fe9267bd394f5e1
Kiwizftw <justinthorby@yahoo.co.nz>
2017-03-07 10:21:05 +1300

    Implemented  a second csv reader for reading a list of boats to compete in a race. -Files take the form: boatName,TeamName
    This method is in a new class called FileReader made specifically for reading files.
    
     #story[5]

--- src/main/java/seng302/App.java
+++ src/main/java/seng302/App.java
@@ -9,21 +9,9 @@ public class App {
      */
     public static void main(String[] args) {
 
-        ArrayList<Boat> ac35 = new ArrayList<>();
-
-        Boat emirates = new Boat("Emirates", "New Zealand");
-        Boat oracle = new Boat("Oracle", "USA");
-        Boat artemis = new Boat("Artemis", "Sweden");
-        Boat groupama = new Boat("Groupama", "France");
-        Boat landRover = new Boat("Land Rover", "Britain");
-        Boat softBank = new Boat("SoftBank", "Japan");
-
-        ac35.add(emirates);
-        ac35.add(oracle);
-//        ac35.add(artemis);
-//        ac35.add(groupama);
-//        ac35.add(landRover);
-//        ac35.add(softBank);
+        FileReader fileReader = new FileReader();
+        ArrayList<Boat> ac35 = fileReader.readBoatListFile("testAc35.txt");
+        System.out.println(ac35);
 
 //        // create a new Course, and then set each mark in the course to have the boats from ac35
 //        Course course = new Course("testCourse.txt");
---
+++ src/main/java/seng302/FileReader.java
@@ -0,0 +1,34 @@
+package seng302;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+/**
+ * Class for reading files
+ */
+class FileReader {
+
+
+    /**
+     * Method to return an ArrayList of boats to compete in a race
+     * @param fileName The name of the file with a list of boats
+     * @return an ArrayList of Boat objects.
+     */
+    ArrayList<Boat> readBoatListFile(String fileName) {
+        ArrayList<Boat> boatList = new ArrayList<>();
+        String line;
+        String csvSplitBy = ",";
+
+        try (BufferedReader b = new BufferedReader(new java.io.FileReader(fileName))) {
+            while ((line = b.readLine()) != null) {
+                String[] boatInfo = line.split(csvSplitBy);
+
+                boatList.add(new Boat(boatInfo[0], boatInfo[1])); //files take form: boatName,teamName
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return boatList;
+    }
+}
