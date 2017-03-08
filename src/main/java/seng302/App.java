2f15ea77753ab93f26258f78fb97d5f825b4fac9
Callum Slaven <csl62@uclive.ac.nz>
2017-03-09 10:55:47 +1300

    Added exception handling in main for invalid input for the race time
    #story[5] csl62

--- src/main/java/seng302/App.java
+++ src/main/java/seng302/App.java
@@ -1,6 +1,7 @@
 package seng302;
 
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class App {
@@ -10,35 +11,43 @@ public class App {
      */
     public static void main(String[] args) {
 
-        //This assumes correct input
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Please enter how long you would like the race to take in minutes");
+        boolean process = true;
 
-        double scaledTime;
-        double time = sc.nextDouble();
-        scaledTime = 1/time;
+        while(process) {
+            try {
+                Scanner sc = new Scanner(System.in);
+                System.out.println("Please enter how long you would like the race to take in minutes");
 
-        FileReader fileReader = new FileReader();
-        ArrayList<Boat> ac35 = fileReader.readBoatListFile("testAc35_1.txt");
-        Course testCourse = new Course("testCourse.txt");
-        Race race = new Race(ac35, testCourse);
+                double scaledTime;
+                double time = sc.nextDouble();
+                scaledTime = 1 / time;
 
-        double speed = 0.539957 * 60 * scaledTime;
+                FileReader fileReader = new FileReader();
+                ArrayList<Boat> ac35 = fileReader.readBoatListFile("testAc35_1.txt");
+                Course testCourse = new Course("testCourse.txt");
+                Race race = new Race(ac35, testCourse);
 
-        for (Boat boat : ac35) {
-            boat.setSpeed(speed);
-            speed += 0;
+                double speed = 0.539957 * 60 * scaledTime;
+
+                for (Boat boat : ac35) {
+                    boat.setSpeed(speed);
+                    speed += 0;
+                }
+
+                System.out.println("-----------------------------COMPETITORS---------------------------------------");
+                race.viewStartingList();
+                System.out.println("-------------------------------------------------------------------------------");
+
+                System.out.println("--------------------------COURSE DESCRIPTION-----------------------------------");
+                testCourse.displayCourse();
+                System.out.println("-------------------------------------------------------------------------------");
+
+                race.runRace();
+                process = false;
+            } catch (InputMismatchException e){
+                System.out.println("The input is invalid, please try again");
+            }
         }
-
-        System.out.println("-----------------------------COMPETITORS---------------------------------------");
-        race.viewStartingList();
-        System.out.println("-------------------------------------------------------------------------------");
-
-        System.out.println("--------------------------COURSE DESCRIPTION-----------------------------------");
-        testCourse.displayCourse();
-        System.out.println("-------------------------------------------------------------------------------");
-
-        race.runRace();
 
     }
 }
