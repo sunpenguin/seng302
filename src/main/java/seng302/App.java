d942af6a995885fca7190a539d35a166fbe71735
Callum Slaven <csl62@uclive.ac.nz>
2017-03-16 14:17:42 +1300

    Created XML course file and XML file reader
       -created the XmlReader class.
       - created an XML file for the course.
       - created compound mark class
       - commented out no longer needed code to get rid of
    
    story[375]

--- src/main/java/seng302/App.java
+++ src/main/java/seng302/App.java
@@ -1,59 +1,38 @@
 package seng302;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import seng302.controller.MainWindowController;
+
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-public class App {
+public class App extends Application {
 
-    /**
-     * The main method for the application
-     */
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+
+
+        FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("MainWindow.fxml"));
+        Parent root = loader.load(); // throws IOException
+        MainWindowController mainWindowController = loader.getController();
+        primaryStage.setTitle("Race Vision");
+        primaryStage.setScene(new Scene(root, 1280, 720));
+        primaryStage.setMinHeight(720);
+        primaryStage.setMinWidth(1280);
+        primaryStage.show();
+
+        mainWindowController.start();
+    }
+
     public static void main(String[] args) {
-
-        boolean process = true;
-
-        while(process) {
-            try {
-                Scanner sc = new Scanner(System.in);
-                System.out.println("Please enter how long you would like the race to take in minutes:");
-
-                double scaledTime;
-                double time = sc.nextDouble();
-
-                scaledTime = 1 / time;
-
-                System.out.println("Please enter how many boats you would like to race:");
-                int numBoats = sc.nextInt();
-
-                FileReader fileReader = new FileReader();
-                ArrayList<Boat> ac35 = fileReader.readBoatListFile("/testAc35_1.txt");
-                Collections.shuffle(ac35); // shuffle the list so that different boats race each time
-
-                Course testCourse = new Course("/testCourse.txt");
-
-                ArrayList<Boat> raceList = new ArrayList<>(ac35.subList(0, numBoats));
-                Race race = new Race(raceList, testCourse);
-
-                double speed = 0.539957 * 60 * scaledTime; // 1 km/h = 0.54 knots
-
-                for (Boat boat : raceList) {
-                    boat.setSpeed(speed);
-                    speed += 5;
-                }
-
-                race.viewStartingList();
-                testCourse.displayCourse();
-                race.runRace();
-
-                process = false;
-            } catch (InputMismatchException e){
-                System.out.println("The input is invalid, please try again");
-            } catch (IndexOutOfBoundsException e){
-                System.out.println("You have selected too many boats! :(");
-            }
-        }
+        launch(args);
     }
 }
 
--- src/main/java/seng302/Mark.java
+++ src/main/java/seng302/Mark.java
@@ -7,118 +7,52 @@ import java.util.ArrayList;
  */
 public class Mark {
 
-    private ArrayList<Boat> boats = new ArrayList<>();
     private String markName;
-    private float markDistance;
-    private float distanceFromStart;
-    private int markPosition;
-    private int markHeading;
+    private double latitude;
+    private double longitude;
 
 
-    /**
-     * Constructor for the Mark class
-     * @param markName The name of the mark
-     * @param markDistance The distance of the leg
-     */
-    Mark(String markName, float markDistance, float distanceFromStart, int markPosition, int markHeading) {
-            this.markName = markName;
-            this.markDistance = markDistance;
-            this.distanceFromStart = distanceFromStart;
-            this.markPosition = markPosition;
-            this.markHeading = markHeading;
+    public Mark(String markName, double latitude, double longitude) {
+        this.markName = markName;
+        this.latitude = latitude;
+        this.longitude = longitude;
     }
 
-
-    /**
-     * A getter for the marks heading
-     * @return The heading of the mark
-     */
-    int getMarkHeading() {
-        return markHeading;
+    public double getLatitude() {
+        return latitude;
     }
 
-
-    /**
-     * A setter for the heading of the mark
-     * @param markHeading The value the marks heading will be set to
-     */
-    void setMarkHeading(int markHeading) {
-        this.markHeading = markHeading;
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
     }
 
-    /**
-     * Getter for the mark's position in the course.
-     * If markPosition = 1, then it is the first mark.
-     * @return the mark position.
-     */
-    int getMarkPosition() {
-        return markPosition;
+    public double getLongitude() {
+        return longitude;
     }
 
-
-    /**
-     * Getter for the length of the leg ending at this mark
-     * @return the length of the leg
-     */
-    float getmarkDistance() {
-        return markDistance;
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
     }
-
-
-    /**
-     * Getter for the distance from the start to the end of the current leg.
-     * Use to decide if a boat has passed the mark.
-     * @return the distance from the start to the end of the leg.
-     */
-    float getDistanceFromStart() {
-        return distanceFromStart;
-    }
-
-
-    /**
-     * A getter to return the variable boats from the Mark
-     * @return The list of boats for the mark
-     */
-    public ArrayList<Boat> getBoats() {
-            return boats;
-        }
-
-
-    /**
-     * Setter the boats at the mark
-     * @param boats Arraylist of boats at the mark
-     */
-    void setBoats(ArrayList<Boat> boats) {
-            this.boats = boats;
-    }
-
 
     /**
      * Getter for the mark name
+     *
      * @return the name of the mark
      */
     String getMarkName() {
-            return markName;
+        return markName;
     }
 
 
     /**
      * A setter for the name of the mark
+     *
      * @param markName The name that the mark will  be set to
      */
     public void setMarkName(String markName) {
-            this.markName = markName;
-        }
-
-
-    /**
-     * A method which displays the order the boats passed through the mark
-     */
-    void displayBoatOrder(){
-        System.out.println(markName);
-        for(int i = 0; i < boats.size(); i++){
-            System.out.println(i+1 + ". " + boats.get(i).getTeamName() + " " + boats.get(i).getBoatName());
-        }
+        this.markName = markName;
     }
+
+
 }
 
