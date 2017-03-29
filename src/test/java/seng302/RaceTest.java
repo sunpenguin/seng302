b87306d4aea5bd371d2efed2afaa0c30c8274bdc
cslaven <csl62@uclive.ac.nz>
2017-03-30 10:26:38 +1300

    Added tests for the race class
    - Tested the updateBoats method to ensure that the boats are moving to the correct coordinate after a certain amount of time
    - Tested the setCourseForBoats to ensure that this works correctly in that the course that is parsed from the XML is set for the boats.
    
    story[375]

--- src/test/java/seng302/RaceTest.java
+++ src/test/java/seng302/RaceTest.java
@@ -1,6 +1,16 @@
 package seng302;
 
 import org.junit.Test;
+import org.xml.sax.SAXException;
+import seng302.model.*;
+import seng302.parser.XMLParser;
+import seng302.raceutil.GPSCalculations;
+
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
 import static org.junit.Assert.*;
 
 
@@ -9,8 +19,51 @@ import static org.junit.Assert.*;
  */
 public class RaceTest {
 
-    @Test
-    public void updateBoatsTest() {
 
+    /**
+     * A test to ensure that the boats, when updated are travelling to the correct position.
+     * @throws IOException
+     * @throws SAXException
+     * @throws ParserConfigurationException
+     */
+    @Test
+    public void updateBoatsTest() throws IOException, SAXException, ParserConfigurationException {
+        int time = 5;
+        File file = new File("src/main/resources/course.xml");
+        XMLParser.parseCourse(file);
+        Course course = XMLParser.parseCourse(file);
+        CompoundMark start = course.getCompoundMarks().get(0);
+        Boat boat1 = new Boat("Emirates", "NZL", 45);
+        ArrayList<Boat> boats = new ArrayList<>();
+        boats.add(boat1);
+        Race race = new Race(boats, course);
+        race.updateBoats(time);
+        double distance = (boat1.getSpeed()/3.6)*time;
+        Coordinate testCoord = GPSCalculations.coordinateToCoordinate(start.getMidCoordinate(),
+                boat1.getHeading(), distance);
+        assertEquals(testCoord.getLatitude(), boat1.getCoordinate().getLatitude(), 0.01);
+        assertEquals(testCoord.getLongitude(), boat1.getCoordinate().getLongitude(), 0.01);
+    }
+
+
+    /**
+     * A test to ensure the course is correctly set for the boat(This method is called inside the race constructor
+     * @throws IOException
+     * @throws SAXException
+     * @throws ParserConfigurationException
+     */
+    @Test
+    public void setCourseForBoatsTest() throws IOException, SAXException, ParserConfigurationException {
+        File file = new File("src/main/resources/course.xml");
+        XMLParser.parseCourse(file);
+        Course course = XMLParser.parseCourse(file);
+        Boat boat1 = new Boat("Emirates", "NZL", 45);
+        ArrayList<Boat> boats = new ArrayList<>();
+        boats.add(boat1);
+        Race testRace = new Race(boats, course);
+        CompoundMark expectedMark = course.getCompoundMarks().get(1);
+        CompoundMark actualMark = boat1.getLeg().getDestination();
+        assertEquals(expectedMark.getMidCoordinate().getLatitude(), actualMark.getMidCoordinate().getLatitude(), 0.01);
+        assertEquals(expectedMark.getMidCoordinate().getLongitude(), actualMark.getMidCoordinate().getLongitude(), 0.01);
     }
 }
