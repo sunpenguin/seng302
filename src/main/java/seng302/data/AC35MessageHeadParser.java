6d915621cf7d9c1e8e066b6f2b93e22fc803002f
Sunguin Peng <spe76@uclive.ac.nz>
2017-04-10 16:32:39 +1200

    Working with ByteBuffers and wrap - Added a method to deal with special cases under ByteCheck. - Did a bit of code cleaning on AC35BoatLocationParser and AC35MessageHeadParser.
    #story[735]

--- src/main/java/seng302/data/AC35MessageHeadParser.java
+++ src/main/java/seng302/data/AC35MessageHeadParser.java
@@ -1,20 +1,27 @@
 package seng302.data;
 
+import seng302.util.ByteCheck;
+
+import java.nio.ByteBuffer;
+
 /**
  * Created by dhl25 on 10/04/17.
  */
 public class AC35MessageHeadParser implements MessageHeadParser {
 
     private final int HEADER_BYTE_SIZE = 15;
-//    private final byte FIRST_SYNC_BYTE = 0x47;
+    //    private final byte FIRST_SYNC_BYTE = 0x47;
 //    private final byte SECOND_SYNC_BYTE = 0x83;
-
+    private final int TYPE_INDEX = 2;
+    private final int LEN_START_INDEX = 13;
+    private final int LEN_LENGTH = 2;
 
     @Override
     public MessageHead parse(byte[] header) {
-        int type = header[2];
+        int type = header[TYPE_INDEX];
         MessageType messageType = MessageType.from(type);
-        int len = ((header[13] & 0xff) << 8) | (header[14] & 0xff);
+        int len = ByteCheck.ByteToIntConverter(header, LEN_START_INDEX, LEN_LENGTH);
+//        ((header[13] & 0xff) << 8) | (header[14] & 0xff);
         return new AC35MessageHead(messageType, len);
     }
 
