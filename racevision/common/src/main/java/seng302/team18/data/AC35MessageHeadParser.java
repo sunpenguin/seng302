f3dafa0c2cba3df7fa2604d598cec0541a272e00
Sunguin Peng <spe76@uclive.ac.nz>
2017-04-11 14:51:52 +1200

    Commit for merge structure - Changed the methods on Boat Location Parser to use ByteCheck method.
    #story[735]

--- racevision/common/src/main/java/seng302/team18/data/AC35MessageHeadParser.java
+++ racevision/common/src/main/java/seng302/team18/data/AC35MessageHeadParser.java
@@ -2,13 +2,15 @@ package seng302.team18.data;
 
 import seng302.team18.util.ByteCheck;
 
+import seng302.util.ByteCheck;
+
 /**
  * Created by dhl25 on 10/04/17.
  */
 public class AC35MessageHeadParser implements MessageHeadParser {
 
     private final int HEADER_BYTE_SIZE = 15;
-    //    private final byte FIRST_SYNC_BYTE = 0x47;
+//    private final byte FIRST_SYNC_BYTE = 0x47;
 //    private final byte SECOND_SYNC_BYTE = 0x83;
     private final int TYPE_INDEX = 2;
     private final int LEN_START_INDEX = 13;
@@ -19,10 +21,9 @@ public class AC35MessageHeadParser implements MessageHeadParser {
         int type = header[TYPE_INDEX];
         AC35MessageType messageType = AC35MessageType.from(type);
         int len = ByteCheck.ByteToIntConverter(header, LEN_START_INDEX, LEN_LENGTH);
-//        ((header[13] & 0xff) << 8) | (header[14] & 0xff);
+//        Old Conversion here: ((header[13] & 0xff) << 8) | (header[14] & 0xff);
         return new AC35MessageHead(messageType, len);
     }
-
 
     @Override
     public int headerSize() {
