ad7f93c286d26a837f7394496a242b7b2ff1377a
Hongting Qiu <hqi19@uclive.ac.nz>
2017-04-11 17:10:28 +1200

    Try to add setUpCourseMap() method in CoordinateContainer class.  It should randomly generate compound marks for a course.
    #story[740]

--- racevision/test_mock/src/main/java/seng302/team18/test_mock/CoordinateContainer.java
+++ racevision/test_mock/src/main/java/seng302/team18/test_mock/CoordinateContainer.java
@@ -3,10 +3,7 @@ package seng302.team18.test_mock;
 import seng302.team18.model.Coordinate;
 import seng302.team18.util.GPSCalculations;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Created by jth102 on 11/04/17.
@@ -14,10 +11,12 @@ import java.util.Map;
 public class CoordinateContainer {
 
     private Map<Integer, List> ACLocationsMap;
+    private Map<String, Coordinate> CourseMap;
 
     public CoordinateContainer() {
         ACLocationsMap = new HashMap();
         setupLocationMap();
+        setUpCourseMap();
     }
 
     private void setupLocationMap() {
@@ -39,5 +38,26 @@ public class CoordinateContainer {
         isleOfWhiteLocations.add(center);
 
         ACLocationsMap.put(1, isleOfWhiteLocations);
+    }
+
+    private void setUpCourseMap() {
+        List<Coordinate> referenceBoundary = ACLocationsMap.get(1);
+
+        Random ran = new Random();
+
+        //startline
+        double rangeStartLat = referenceBoundary.get(4).getLatitude() - referenceBoundary.get(2).getLatitude();
+        double startLine1Lat = ran.nextDouble() * rangeStartLat + referenceBoundary.get(2).getLatitude();
+        double rangeStartLon = referenceBoundary.get(4).getLongitude() - referenceBoundary.get(2).getLongitude();
+        double startLine1Lon = ran.nextDouble() * rangeStartLon + referenceBoundary.get(2).getLongitude();
+        Coordinate startLine1 = new Coordinate(startLine1Lat, startLine1Lon);
+        CourseMap.put("StartLine1", startLine1);
+        Coordinate startLine2 = GPSCalculations.coordinateToCoordinate(startLine1,100,1212);
+        CourseMap.put("StartLine2", startLine2);
+
+        // mark
+        double rangeMarkLat = referenceBoundary.get(4).getLatitude();
+        double rangeMarkLon = referenceBoundary.get(4).getLongitude();
+        CourseMap.put("Mark", new Coordinate(rangeMarkLat, rangeMarkLon));
     }
 }
