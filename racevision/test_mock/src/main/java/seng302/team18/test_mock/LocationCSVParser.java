5ac746e3a327e3ab46ea5e3bb72b484cac01261b
Sam Beeston <sbe67@uclive.ac.nz>
2017-04-12 14:05:08 +1200

    Getting all information for regatta.xml and calculating centre
    #story[740]

--- racevision/test_mock/src/main/java/seng302/team18/test_mock/LocationCSVParser.java
+++ racevision/test_mock/src/main/java/seng302/team18/test_mock/LocationCSVParser.java
@@ -4,13 +4,19 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
+
+import seng302.team18.model.Coordinate;
+import seng302.team18.util.GPSCalculations;
 
 /**
  * Class to parse race data from a CSV.
  * Data is to be used to create Regatta.xml.
  * CSV should have lines in the format of:
- * Regatta ID, Regatta Name, Course Name, UTC, Magnetic Variation, Race ID, Race Type, Race Start Time, top left Lat, top left long, top right Lat, top right long, bottom right Lat, bottom right long, top left Lat, top left long
+ * Regatta ID, Regatta Name, Course Name, UTC, Magnetic Variation, Race ID, Race Type, Race Start Time, top left Lat, top left long, top right Lat, top right long, bottom right Lat, bottom right long, bottom left Lat, bottom left long
  */
 public class LocationCSVParser {
 
@@ -19,21 +25,27 @@ public class LocationCSVParser {
      * @param file CSV file
      */
     public static void ParserCSV(File file) {
+        //get number of lines
         int numRaces = 0;
         try {
-            numRaces = getNumRaces(file);           //get number of lines
+            numRaces = getNumRaces(file);
             System.out.println();
         }catch (Exception e){
         }
+
+        //chose a random line
         Random rand = new Random();
-        int  lineNum = rand.nextInt(numRaces);      //chose a random line
+        int  lineNum = rand.nextInt(numRaces);
+
+        //get chosen line
         String location = "oh no";
         try {
-            location = getLine(lineNum, file);      //get chosen line
+            location = getLine(lineNum, file);
         }catch (Exception e){
         }
-        System.out.println(lineNum + "  " +location);
-        //makeRegatta(location);                      //create Regatta.xml
+
+        //create Regatta.xml
+        makeRegatta(location);
     }
 
     //gets the number of lines in a file
@@ -61,8 +73,46 @@ public class LocationCSVParser {
         return line;
     }
 
-//    private static File makeRegatta(String location){
-//
-//    }
+    //creates the regatta.xml file
+    private static void makeRegatta(String location){
+        //split the line into its items
+        List<String> locationsInfoList = Arrays.asList(location.split(", "));
+        String regattaID = locationsInfoList.get(0);
+        String regattaName = locationsInfoList.get(1);
+        String courseName = locationsInfoList.get(2);
+        String UTC = locationsInfoList.get(3);
+        String magneticVariation = locationsInfoList.get(4);
+        String raceID = locationsInfoList.get(5);
+        String raceType = locationsInfoList.get(6);
+        String raceStartTime = locationsInfoList.get(7);
+        double latTL = new Double(locationsInfoList.get(8));
+        double longTL = new Double(locationsInfoList.get(9));
+        double latTR = new Double(locationsInfoList.get(10));
+        double longTR = new Double(locationsInfoList.get(11));
+        double latBR = new Double(locationsInfoList.get(12));
+        double longBR = new Double(locationsInfoList.get(13));
+        double latBL = new Double(locationsInfoList.get(14));
+        double longBL = new Double(locationsInfoList.get(15));
+
+        //Create Co-ordinates
+        Coordinate topleft = new Coordinate(latTL, longTL);
+        Coordinate topRight = new Coordinate(latTR, longTR);
+        Coordinate bottomRight = new Coordinate(latBR, longBR);
+        Coordinate bottomLeft = new Coordinate(latBL, longBL);
+
+        //Create Co-ordinates list
+        List<Coordinate> locationsCoordinates = new ArrayList();
+        locationsCoordinates.add(topleft);
+        locationsCoordinates.add(topRight);
+        locationsCoordinates.add(bottomRight);
+        locationsCoordinates.add(bottomLeft);
+
+        //get centre co-ordinate for XML
+        Coordinate center = GPSCalculations.getCentralCoordinate(locationsCoordinates);
+
+//        System.out.println(locationsCoordinates);
+//        System.out.println(center.getLatitude() + "  " + center.getLongitude());
+
+    }
 
 }
