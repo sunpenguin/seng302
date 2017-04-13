91c34825858bb40254bf401a4086652adbec10cf
Sam Beeston <sbe67@uclive.ac.nz>
2017-04-14 11:44:35 +1200

    Regatta.xml if being created, on multiple lines.
    #story[740]

--- racevision/test_mock/src/main/java/seng302/team18/test_mock/LocationCSVParser.java
+++ racevision/test_mock/src/main/java/seng302/team18/test_mock/LocationCSVParser.java
@@ -18,6 +18,7 @@ import seng302.team18.util.GPSCalculations;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
@@ -180,6 +181,8 @@ public class LocationCSVParser {
             // write the content into xml file
             TransformerFactory transformerFactory = TransformerFactory.newInstance();
             Transformer transformer = transformerFactory.newTransformer();
+            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+            transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "2");
             DOMSource source = new DOMSource(doc);
             StreamResult result = new StreamResult(new File("racevision/test_mock/src/main/resources/Regatta.xml"));
 
