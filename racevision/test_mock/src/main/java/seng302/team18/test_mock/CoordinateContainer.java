04fd2bb040ea8f9e28ec7838c1f567668a822575
hqi19 <hqi19@uclive.ac.nz>
2017-04-13 20:45:19 +1200

    The setUpCourse() method now can generate compound marks for race in a xml file.
    #story[740]

--- racevision/test_mock/src/main/java/seng302/team18/test_mock/CoordinateContainer.java
+++ racevision/test_mock/src/main/java/seng302/team18/test_mock/CoordinateContainer.java
@@ -8,10 +8,7 @@ import seng302.team18.util.GPSCalculations;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerConfigurationException;
-import javax.xml.transform.TransformerException;
-import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.*;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 import java.io.File;
@@ -118,8 +115,8 @@ public class CoordinateContainer {
 
             Element startTime = doc.createElement("RaceStartTime");
             race.appendChild(startTime);
-            startTime.setAttribute("Postpone","false");
             startTime.setAttribute("Time","2011-08-06T13:30:00-0700");
+            startTime.setAttribute("Postpone","false");
 
             Element participants = doc.createElement("Participants");
             race.appendChild(participants);
@@ -129,8 +126,8 @@ public class CoordinateContainer {
             participants.appendChild(yacht1);
             participants.appendChild(yacht2);
 
-            yacht1.setAttribute("Entry","Port");
             yacht1.setAttribute("SourceID","107");
+            yacht1.setAttribute("Entry","Port");
             yacht2.setAttribute("Entry","Stbd");
             yacht2.setAttribute("SourceID","108");
 
@@ -147,12 +144,12 @@ public class CoordinateContainer {
             compoundMark1.appendChild(s2);
             s1.setAttribute("SourceID","101");
             s1.setAttribute("TargetLng", String.valueOf(CourseMap.get("StartLine1").getLongitude()));
-            s1.setAttribute("TargetLat", String.valueOf(CourseMap.get("StartLine1").getLongitude()));
+            s1.setAttribute("TargetLat", String.valueOf(CourseMap.get("StartLine1").getLatitude()));
             s1.setAttribute("Name","PRO");
             s1.setAttribute("SeqID","1");
             s2.setAttribute("SourceID","102");
             s2.setAttribute("TargetLng", String.valueOf(CourseMap.get("StartLine2").getLongitude()));
-            s2.setAttribute("TargetLat", String.valueOf(CourseMap.get("StartLine2").getLongitude()));
+            s2.setAttribute("TargetLat", String.valueOf(CourseMap.get("StartLine2").getLatitude()));
             s2.setAttribute("Name","PIN");
             s2.setAttribute("SeqID","2");
 
@@ -161,13 +158,88 @@ public class CoordinateContainer {
             compoundMark2.setAttribute("Name","M1");
             compoundMark2.setAttribute("CompoundMarkID","2");
             Element m1 = doc.createElement("Mark");
+            compoundMark2.appendChild(m1);
+            m1.setAttribute("SourceID","103");
+            m1.setAttribute("TargetLng", String.valueOf(CourseMap.get("Mark1").getLongitude()));
+            m1.setAttribute("TargetLat", String.valueOf(CourseMap.get("Mark1").getLatitude()));
+            m1.setAttribute("Name","M1");
+
+            Element compoundMark3 = doc.createElement("CompoundMark");
+            course.appendChild(compoundMark3);
+            compoundMark3.setAttribute("Name","M2");
+            compoundMark3.setAttribute("CompoundMarkID","3");
+            Element m2 = doc.createElement("Mark");
+            compoundMark3.appendChild(m2);
+            m2.setAttribute("SourceID","104");
+            m2.setAttribute("TargetLng", String.valueOf(CourseMap.get("Mark2").getLongitude()));
+            m2.setAttribute("TargetLat", String.valueOf(CourseMap.get("Mark2").getLatitude()));
+            m2.setAttribute("Name","M2");
+
+            Element compoundMark4 = doc.createElement("CompoundMark");
+            course.appendChild(compoundMark4);
+            compoundMark4.setAttribute("Name","Gate");
+            compoundMark4.setAttribute("CompoundMarkID","4");
+
+            Element g1 = doc.createElement("Mark");
+            Element g2 = doc.createElement("Mark");
+            compoundMark4.appendChild(g1);
+            compoundMark4.appendChild(g2);
+            g1.setAttribute("SourceID","104");
+            g1.setAttribute("TargetLng", String.valueOf(CourseMap.get("Gate1").getLongitude()));
+            g1.setAttribute("TargetLat", String.valueOf(CourseMap.get("Gate1").getLatitude()));
+            g1.setAttribute("Name","G1");
+            g1.setAttribute("SeqID","1");
+            g2.setAttribute("SourceID","105");
+            g2.setAttribute("TargetLng", String.valueOf(CourseMap.get("Gate2").getLongitude()));
+            g2.setAttribute("TargetLat", String.valueOf(CourseMap.get("Gate2").getLatitude()));
+            g2.setAttribute("Name","G2");
+            g2.setAttribute("SeqID","2");
+
+            Element compoundMarkSeq = doc.createElement("CompoundMarkSequence");
+            race.appendChild(compoundMarkSeq);
+            Element compoundMarkSeq1 = doc.createElement("Corner");
+            compoundMarkSeq.appendChild(compoundMarkSeq1);
+            compoundMarkSeq1.setAttribute("SeqID","1");
+            compoundMarkSeq1.setAttribute("CompoundMarkID","1");
+            compoundMarkSeq1.setAttribute("Rounding","SP");
+            compoundMarkSeq1.setAttribute("ZoneSize","3");
+
+            Element compoundMarkSeq2 = doc.createElement("Corner");
+            compoundMarkSeq.appendChild(compoundMarkSeq2);
+            compoundMarkSeq2.setAttribute("SeqID","2");
+            compoundMarkSeq2.setAttribute("CompoundMarkID","2");
+            compoundMarkSeq2.setAttribute("Rounding","Port");
+            compoundMarkSeq2.setAttribute("ZoneSize","3");
+
+            Element compoundMarkSeq3 = doc.createElement("Corner");
+            compoundMarkSeq.appendChild(compoundMarkSeq3);
+            compoundMarkSeq3.setAttribute("SeqID","3");
+            compoundMarkSeq3.setAttribute("CompoundMarkID","3");
+            compoundMarkSeq3.setAttribute("Rounding","Stbd");
+            compoundMarkSeq3.setAttribute("ZoneSize","6");
+
+            Element compoundMarkSeq4 = doc.createElement("Corner");
+            compoundMarkSeq.appendChild(compoundMarkSeq4);
+            compoundMarkSeq4.setAttribute("SeqID","4");
+            compoundMarkSeq4.setAttribute("CompoundMarkID","4");
+            compoundMarkSeq4.setAttribute("Rounding","PS");
+            compoundMarkSeq4.setAttribute("ZoneSize","6");
+
+            Element compoundMarkSeq5 = doc.createElement("Corner");
+            compoundMarkSeq.appendChild(compoundMarkSeq5);
+            compoundMarkSeq5.setAttribute("SeqID","5");
+            compoundMarkSeq5.setAttribute("CompoundMarkID","1");
+            compoundMarkSeq5.setAttribute("Rounding","SP");
+            compoundMarkSeq5.setAttribute("ZoneSize","3");
 
             TransformerFactory transformerFactory = TransformerFactory.newInstance();
             try {
                 Transformer transformer = transformerFactory.newTransformer();
+                transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+                transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "2");
                 DOMSource source = new DOMSource(doc);
-                StreamResult result =  new StreamResult(System.out);
-//                StreamResult result = new StreamResult(new File("C:\\file.xml"));
+//                StreamResult result =  new StreamResult(System.out);
+                StreamResult result = new StreamResult(new File("C:\\Users\\Public\\team-18\\racevision\\common\\src\\main\\resources\\file.xml"));
                 try {
                     transformer.transform(source, result);
                 } catch (TransformerException e) {
