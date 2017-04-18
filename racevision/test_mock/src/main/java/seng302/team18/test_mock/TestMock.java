4f1855966f90d4f2b9830dd96a9b6af55f3467d9
Justin Thorby <jth102@uclive.ac.nz>
2017-04-19 11:36:03 +1200

    Started building regatta xml parser
     #story[740]

--- racevision/test_mock/src/main/java/seng302/team18/test_mock/TestMock.java
+++ racevision/test_mock/src/main/java/seng302/team18/test_mock/TestMock.java
@@ -3,6 +3,7 @@ package seng302.team18.test_mock;
 import seng302.team18.model.Course;
 import seng302.team18.model.Race;
 import seng302.team18.model.Regatta;
+import seng302.team18.test_mock.XMLparsers.AC35RegattaParser;
 
 /**
  * Created by Justin on 18/04/2017.
@@ -14,10 +15,11 @@ public class TestMock {
     private Race race;
 
     private void readFiles() {
-
+        AC35RegattaParser ac35RegattaParser = new AC35RegattaParser();
+        ac35RegattaParser.parse(this.getClass().getResourceAsStream("/AC35Regatta.xml"));
     }
 
     public void run() {
-
+        readFiles();
     }
 }
--- racevision/test_mock/src/main/java/seng302/team18/test_mock/XMLparsers/AC35RegattaParser.java
+++ racevision/test_mock/src/main/java/seng302/team18/test_mock/XMLparsers/AC35RegattaParser.java
@@ -1,7 +1,26 @@
 package seng302.team18.test_mock.XMLparsers;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.InputStream;
+
 /**
  * Created by jth102 on 19/04/17.
  */
 public class AC35RegattaParser {
+
+    public void parse(InputStream regattaFile) {
+
+        try {
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dbuilder = dbFactory.newDocumentBuilder();
+
+
+        } catch (ParserConfigurationException pce) {
+            pce.printStackTrace();
+        }
+
+
+    }
 }
