87c388f583934489605af44122f3086c484e617e
Justin Thorby <jth102@uclive.ac.nz>
2017-04-19 11:19:47 +1200

    Set up a xml parser for each AC35 file. (not implemented yet)
     #story[740]

--- racevision/test_mock/src/main/java/seng302/team18/test_mock/TestMock.java
+++ racevision/test_mock/src/main/java/seng302/team18/test_mock/TestMock.java
@@ -1,9 +1,21 @@
 package seng302.team18.test_mock;
 
+import seng302.team18.model.Course;
+import seng302.team18.model.Race;
+import seng302.team18.model.Regatta;
+
 /**
  * Created by Justin on 18/04/2017.
  */
 public class TestMock {
+
+    private Regatta regatta;
+    private Course course;
+    private Race race;
+
+    private void readFiles() {
+
+    }
 
     public void run() {
 
---
+++ racevision/test_mock/src/main/java/seng302/team18/test_mock/XMLparsers/AC35BoatsParser.java
@@ -0,0 +1,8 @@
+package seng302.team18.test_mock.XMLparsers;
+
+/**
+ * Created by jth102 on 19/04/17.
+ */
+public class AC35BoatsParser {
+
+}
