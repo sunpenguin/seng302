07bed86efad8de316ebfd51b9325fb63ad059a5b
Justin Thorby <jth102@uclive.ac.nz>
2017-04-20 10:32:42 +1200

    no notable change

--- racevision/test_mock/src/main/java/seng302/team18/test_mock/TestMock.java
+++ racevision/test_mock/src/main/java/seng302/team18/test_mock/TestMock.java
@@ -3,6 +3,7 @@ package seng302.team18.test_mock;
 import seng302.team18.model.Course;
 import seng302.team18.model.Race;
 import seng302.team18.model.Regatta;
+import seng302.team18.test_mock.XMLparsers.AC35RaceParser;
 import seng302.team18.test_mock.XMLparsers.AC35RegattaContainer;
 import seng302.team18.test_mock.XMLparsers.AC35RegattaParser;
 
@@ -12,11 +13,13 @@ import seng302.team18.test_mock.XMLparsers.AC35RegattaParser;
 public class TestMock {
 
     AC35RegattaContainer ac35RegattaContainer;
-//    private Race race = new Race();
 
     private void readFiles() {
         AC35RegattaParser ac35RegattaParser = new AC35RegattaParser();
         ac35RegattaContainer = ac35RegattaParser.parse(this.getClass().getResourceAsStream("/AC35regatta.xml"));
+
+        AC35RaceParser ac35RaceParser = new AC35RaceParser();
+
 
     }
 
