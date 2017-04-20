a49de6e187cec9b7acfcf65c1a772acf6292d9f5
hqi19 <hqi19@uclive.ac.nz>
2017-04-20 20:34:13 +1200

    Add 4 unit tests for AC35BoatsParser. Add docs for AC35RaceContainer and AC35BoatsContainer class.
    #story[740]

--- racevision/test_mock/src/main/java/seng302/team18/test_mock/XMLparsers/AC35RaceContainer.java
+++ racevision/test_mock/src/main/java/seng302/team18/test_mock/XMLparsers/AC35RaceContainer.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Map;
 
 /**
- * Created by hqi19 on 20/04/17.
+ * Class that contains all information for a race.
  */
 public class AC35RaceContainer {
     private String startTime;
@@ -19,42 +19,72 @@ public class AC35RaceContainer {
     private List<Integer> participantIDs;
     private List<MarkRounding> markRoundings;
 
+    /*
+    Get start time for a race.
+     */
     public String getStartTime() {
         return startTime;
     }
 
+    /*
+    Set the start time of a race.
+     */
     public void setStartTime(String startTime) {
         this.startTime = startTime;
     }
 
+    /*
+    Get a list of boundary marks.
+     */
     public List<BoundaryMark> getBoundaryMark() {
         return boundaryMark;
     }
 
+    /*
+    Set the value of a list of boundary marks.
+     */
     public void setBoundaryMark(List<BoundaryMark> boundaryMark) {
         this.boundaryMark = boundaryMark;
     }
 
+    /*
+    Get a map of compound marks with its ID as key.
+     */
     public Map<Integer, CompoundMark> getCompoundMarks() {
         return compoundMarks;
     }
 
+    /*
+    Set up a map compound marks with its ID as key.
+     */
     public void setCompoundMarks(Map<Integer, CompoundMark> compoundMarks) {
         this.compoundMarks = compoundMarks;
     }
 
+    /*
+    Get a list of IDs for the participanting yachts.
+     */
     public List<Integer> getParticipantIDs() {
         return participantIDs;
     }
 
+    /*
+    Set up a list of IDs for the participanting yachts.
+     */
     public void setParticipantIDs(List<Integer> participantIDs) {
         this.participantIDs = participantIDs;
     }
 
+    /*
+    Get a list of mark roundings.
+     */
     public List<MarkRounding> getMarkRoundings() {
         return markRoundings;
     }
 
+    /*
+    Set up a list of mark roundings.
+     */
     public void setMarkRoundings(List<MarkRounding> markRoundings) {
         this.markRoundings = markRoundings;
     }
