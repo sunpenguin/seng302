b41777d965d9f1c88b5319b16fd67702fecbdfcc
Hongting Qiu <hqi19@uclive.ac.nz>
2017-04-20 12:52:07 +1200

    Add test for regatta parser.
    #story[740]

---
+++ racevision/test_mock/src/test/java/AC35RegattaParserTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by hqi19 on 20/04/17.
+ */
+public class AC35RegattaParserTest {
+
+    @Test
+    public void testParser() {
+        assertEquals(true, true);
+    }
+}
