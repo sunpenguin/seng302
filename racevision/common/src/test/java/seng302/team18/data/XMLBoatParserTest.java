d104d3e854348b780e4dcd3d49d4997a14f99574
Jessica Slaney <jds112@uclive.ac.nz>
2017-04-24 15:24:38 +1200

    -Boat xml test work, commented out as is not working
    #test

--- racevision/common/src/test/java/seng302/team18/data/XMLBoatParserTest.java
+++ racevision/common/src/test/java/seng302/team18/data/XMLBoatParserTest.java
@@ -1,34 +1,44 @@
-package seng302.team18.data;
-
-import org.junit.Test;
-import org.xml.sax.SAXException;
-import seng302.team18.model.Boat;
-
-import javax.xml.parsers.ParserConfigurationException;
-import java.io.BufferedInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.List;
-
-import static org.junit.Assert.assertEquals;
-
-/**
- * Created by dhl25 on 30/03/17.
- */
-public class XMLBoatParserTest {
-
-//    private InputStream boatFile = new BufferedInputStream(getClass().getResourceAsStream("/test-boats.xml"));
-
-//    @Test
-//    public void testParseBoats() throws IOException, SAXException, ParserConfigurationException {
+//package seng302.team18.data;
 //
-//        List<Boat> actual = XMLBoatParser.parseBoats(boatFile);
+//import org.junit.Before;
+//import org.junit.Test;
+//import org.xml.sax.SAXException;
+//import seng302.team18.model.Boat;
+//
+//import javax.xml.parsers.ParserConfigurationException;
+//import java.io.BufferedInputStream;
+//import java.io.IOException;
+//import java.io.InputStream;
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//import static org.junit.Assert.assertEquals;
+//
+///**
+// * Created by dhl25 on 30/03/17.
+// */
+//public class XMLBoatParserTest {
+//
+//    private InputStream boatFile;
+//    private List<Boat> actual;
+//
+//    @Before
+//    public void setUp() throws IOException, SAXException, ParserConfigurationException {
+//        boatFile = new BufferedInputStream(getClass().getResourceAsStream("/boatsTest.xml"));
+//        actual = XMLBoatParser.parseBoats(boatFile);
+//    }
+//
+//    @Test
+//    public void testParseBoats() throws IOException, SAXException, ParserConfigurationException {;
 //        List<Boat> expected = new ArrayList<>();
-//        expected.add(new Boat("Emirates Team New Zealand", "NZL", 45.0));
-//        expected.add(new Boat("Oracle Team USA", "USA", 50));
-//        expected.add(new Boat("Artemis Racing", "SWE", 55.0));
+//        expected.add(new Boat("Emirates Team New Zealand", "NZL", 103));
+//        expected.add(new Boat("Oracle Team USA", "USA", 101));
+//        expected.add(new Boat("Artemis Racing", "SWE", 102));
+//
+//        System.out.println(expected.size());
+//        System.out.println(actual.size());
 //        assertEquals(expected.size(), actual.size());
+//
 //        for (int i = 0; i < expected.size(); i++) {
 //            Boat exp = expected.get(i);
 //            Boat act = actual.get(i);
@@ -42,5 +52,7 @@ public class XMLBoatParserTest {
 //            assertEquals(exp.getSpeed(), act.getSpeed(), 0.1);
 //        }
 //    }
-
-}
+//
+//}
+//
+//
