fb769f9971ff28aec85c91396c034e1927fddd0f
Sam Beeston <sbe67@uclive.ac.nz>
2017-05-01 16:35:46 +1200

    Deleted old files and removed comments

--- racevision/visualiser/src/main/java/seng302/team18/visualiser/Main.java
+++ racevision/visualiser/src/main/java/seng302/team18/visualiser/Main.java
@@ -12,27 +12,11 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-
-
-//        FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("MainWindow.fxml"));
-////        FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("PreRace.fxml"));
-//        Parent root = loader.load(); // throws IOException
-//        MainWindowController mainWindowController = loader.getController();
-////        PreRaceController preRaceController = loader.getController();
-//        primaryStage.setTitle("RaceVision");
-//        Scene scene = new Scene(root, 1280, 720);
-//        primaryStage.setScene(scene);
-//        primaryStage.show();
-
-
         ControllerManager manager = new ControllerManager(primaryStage, "MainWindow.fxml", "PreRace.fxml");
         manager.start();
     }
 
     public static void main(String[] args) {
-//        ControllerManager manager = new ControllerManager("MainWindow.fxml");
-//        ControllerManager manager = new ControllerManager("MainWindow.fxml", "PreRace.fxml");
-//        manager.startApp(args);
         launch(args);
     }
 }
