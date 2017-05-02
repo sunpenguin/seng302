6a27b402f58304ae17d21dab8744604862da819e
Hongting Qiu <hqi19@uclive.ac.nz>
2017-05-02 17:38:41 +1200

    Delete GenerateCourse class. Remove unused lines in CRCGenerator.
    #story[741]

--- racevision/test_mock/src/main/java/seng302/team18/test_mock/connection/CRCGenerator.java
+++ racevision/test_mock/src/main/java/seng302/team18/test_mock/connection/CRCGenerator.java
@@ -15,9 +15,6 @@ public class CRCGenerator {
     public static byte[] generateCRC(byte[] message) throws IOException {
 
         // calculate the checksum for given message
-//        Checksum checksum = new CRC32();
-//        checksum.update(message, 0, message.length);
-//        long checksumValue = checksum.getValue();
 
         int crc  = 0xFFFFFFFF;       // initial contents of LFBSR
         int poly = 0xEDB88320;   // reverse polynomial
@@ -40,67 +37,6 @@ public class CRCGenerator {
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         outputStream.write(value);
         byte[] crcValue = outputStream.toByteArray();
-//        System.out.println(ByteCheck.byteToIntConverter(value, 0, 4));
-//
-//        System.out.println("CRC32 (via direct calculation) = " + Integer.toHexString(crc));
-
-//        String s = "";
-//        for (Byte b: message) {
-//            s += b.toString();
-//        }
-//        System.out.println(s);
-
-//        byte[] bytes = ByteBuffer.allocate(Long.SIZE / Byte.SIZE).putLong(checksumValue).array();
-//        System.out.println(bytes+" length:8");
-
-//        String check = Long.toHexString(checksumValue);
-//        byte[] crc = check.getBytes();
-//        System.out.println(check);
-//        byte[] value = new byte[4];
-//        value[0] = (byte) (crc[0] + crc[1]);
-//        value[1] = (byte) (crc[2] + crc[3]);
-//        value[2] = (byte) (crc[4] + crc[5]);
-//        value[3] = (byte) (crc[6] + crc[7]);
-//        System.out.println(value);
-
-//        System.out.println(c);
-//        System.out.println(crcValue);
-//        ByteCheck.byteToLongConverter(crcValue,0,)
-
-//        byte[] value = toByteArray(checksumValue);
-//        System.out.println(value+" length: "+value.length);
-
-//        String va = "";
-//        for (Byte v: value) {
-//            va += v.toString();
-//            System.out.println(v);
-//        }
-//        System.out.println(va);
-//        System.out.println(ByteCheck.byteToLongConverter(bytes, 0, 4));
-
-//        byte[] check = ByteCheck.convertLongTo6ByteArray(checksumValue);
-//        System.out.println(check);
-//        System.out.println(ByteCheck.convertToLittleEndian(check, 4));
-//        byte[] result = new byte[4];
-//        System.arraycopy(string.getBytes(), 0, result, 100 - string.length(), string.length());
-//        byte[] bytes = ByteBuffer.allocate(4).putLong(checksumValue).array();
-//        System.out.println(bytes);
-
-
-        // return a byte array
-//        ByteBuffer buffer = ByteBuffer.allocate(Long.BYTES);
-//        buffer.putLong(checksumValue);
-//
-//        byte[] result = new byte[4];
-//        System.arraycopy(buffer.array(), 0, result, 0, 4);
-//
-//        for(int i = 0; i < result.length / 2; i++)
-//        {
-//            byte temp = result[i];
-//            result[i] = result[result.length - i - 1];
-//            result[result.length - i - 1] = temp;
-//        }
-//        System.out.println(result);
         return ByteCheck.convertToLittleEndian(crcValue, 4);
     }
 
