efd9524be6547cc508edc7b8ccad4b738036ec5e
Sam Beeston <sbe67@uclive.ac.nz>
2017-05-02 17:25:05 +1200

    Started Polar Parsing
    -Setup PolarCalculator class to be implemented into the test mock
    -PolarCalculator doesn't do what it is intended do but is a placeholder
    -the Polar class handles the polar data for a singular windspeed
    -PolarParser parses data for the new file from Thomas
    
    #story[737]

---
+++ racevision/common/src/main/java/seng302/team18/util/PolarCalculator.java
@@ -0,0 +1,16 @@
+package seng302.team18.util;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by sbe67 on 2/05/17.
+ */
+public class PolarCalculator {
+
+    public static List<Double> getBest(double windSpeed, double heading, double windAngle){
+        double offset = 0;
+        return new ArrayList<Double>(Arrays.asList(windSpeed, 0.0));
+    }
+}
