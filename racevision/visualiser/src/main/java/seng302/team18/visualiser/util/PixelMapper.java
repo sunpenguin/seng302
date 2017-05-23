27f3b4eecafd876fb0f7e7129f78a03c5e2698fd
Justin Thorby <jth102@uclive.ac.nz>
2017-05-23 16:02:41 +1200

    Updated documentation in the PixelMapper class. - Added Javadoc - Also split the coordToPixel method into 2 methods as it was quite long
    #story[937]

--- racevision/visualiser/src/main/java/seng302/team18/visualiser/util/PixelMapper.java
+++ racevision/visualiser/src/main/java/seng302/team18/visualiser/util/PixelMapper.java
@@ -20,7 +20,6 @@ import java.util.List;
 public class PixelMapper {
 
     private PropertyChangeSupport propertyChangeSupport = new PropertyChangeSupport(this);
-
     private final Course course;
     private final Pane pane;
     private Coordinate viewPortCenter;
@@ -52,18 +51,15 @@ public class PixelMapper {
         propertyChangeSupport.removePropertyChangeListener("viewPortCenter", listener);
     }
 
+    /**
+     * Update the view port center to a certain GPS coordinate.
+     *
+     * @param center The target location to center the view on. (Example: A boat or mark location)
+     */
     public void setViewPortCenter(Coordinate center) {
         Coordinate old = viewPortCenter;
         viewPortCenter = center;
         propertyChangeSupport.firePropertyChange("viewPortCenter", old, center);
-    }
-
-    public IntegerProperty zoomLevelProperty() {
-        return zoomLevel;
-    }
-
-    public void setZoomLevel(int level) {
-        zoomLevel.set(level);
     }
 
     /**
@@ -76,16 +72,11 @@ public class PixelMapper {
     }
 
     /**
-     * Maps a coordinate to a pixel value relative to the current resolution and zoom of the race view pane
-     * NOTE: Origin is at the top left corner (X and Y increase to the right and downwards respectively)
+     * Update values that are set by talking to the Google Maps JavaScript API
      *
-     * @param coord Coordinate to map
-     * @return XYPair containing the x and y pixel values
+     * @param coord The coordinate getting converted to pixel coordinates
      */
-    public XYPair coordToPixel(Coordinate coord) {
-
-        IntegerProperty mapZoom = new SimpleIntegerProperty(0);
-
+    private void updateMapsValues(Coordinate coord) {
         try {
             worldCoordinatesCartesian = getPixelsFromGoogle(coord);
             viewCenterCartesian = getPixelsFromGoogle(viewPortCenter);
@@ -104,6 +95,18 @@ public class PixelMapper {
         } catch (Exception e) {
             // The maps have not loaded yet
         }
+    }
+
+    /**
+     * Maps a coordinate to a pixel value relative to the current resolution and zoom of the race view pane
+     * NOTE: Origin is at the top left corner (X and Y increase to the right and downwards respectively)
+     *
+     * @param coord Coordinate to map
+     * @return XYPair containing the x and y pixel values
+     */
+    public XYPair coordToPixel(Coordinate coord) {
+
+        updateMapsValues(coord);
 
         double courseWidth = calcCourseWidth();
         double courseHeight = calcCourseHeight();
@@ -123,17 +126,27 @@ public class PixelMapper {
             mappingScale = paneHeight / courseHeight;
         }
 
-//        mappingScale *= Math.pow(2, zoomLevel.intValue());
-//        if(mappingScale != 1){
-//            mappingScale = mappingScale/2;
-//        }
-
         double dX = worldCoordinatesCartesian.getX() - viewCenterCartesian.getX();
         double dY = worldCoordinatesCartesian.getY() - viewCenterCartesian.getY();
 
         double x = dX * mappingScale + paneWidth / 2;
         double y = dY * mappingScale + paneHeight / 2;
 
+        return new XYPair(x, y);
+    }
+
+    /**
+     * Retrieves a pixel value for a GPS coordinate.
+     * Values are retrieved by talking to the Google Maps JavaScript API
+     * @param coord Coordinate to retrieve pixel value for
+     * @return an XYPair containing the x and y pixel coordinates
+     * @throws Exception
+     */
+    private XYPair getPixelsFromGoogle(Coordinate coord) throws Exception {
+        double x = ((double) webEngine.executeScript("convertCoordX(" + coord.getLatitude() + ","
+                + coord.getLongitude() + ");"));
+        double y = ((double) webEngine.executeScript("convertCoordY(" + coord.getLatitude() + ","
+                + coord.getLongitude() + ");"));
         return new XYPair(x, y);
     }
 
@@ -155,17 +168,21 @@ public class PixelMapper {
         return Math.abs(nwBound.getY() - nwBound.getY());
     }
 
+    /**
+     *
+     * Set the bounds of the map view.
+     * @param bounds List of 2 coordinates where index 0 is the north west bound and
+     *               index 1 is the south east bound
+     */
     public void setBounds(List<Coordinate> bounds) {
         this.bounds = bounds;
     }
 
-    private XYPair getPixelsFromGoogle(Coordinate coord) throws Exception {
-        double x = ((double) webEngine.executeScript("convertCoordX(" + coord.getLatitude() + ","
-                + coord.getLongitude() + ");"));
-        double y = ((double) webEngine.executeScript("convertCoordY(" + coord.getLatitude() + ","
-                + coord.getLongitude() + ");"));
-        return new XYPair(x, y);
+    public IntegerProperty zoomLevelProperty() {
+        return zoomLevel;
+    }
+
+    public void setZoomLevel(int level) {
+        zoomLevel.set(level);
     }
 }
-
-
