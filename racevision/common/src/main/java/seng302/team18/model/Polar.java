6529da05449fef22a168602ef94f6b34faa8ddde
Sam Beeston <sbe67@uclive.ac.nz>
2017-06-30 16:25:38 +1200

    Trying to make CI bot not fail
    -Intellij is currently not working correctly :(
    
    #Story[1061]

--- racevision/common/src/main/java/seng302/team18/model/Polar.java
+++ racevision/common/src/main/java/seng302/team18/model/Polar.java
@@ -7,19 +7,63 @@ import java.util.Map;
  */
 public class Polar {
 
-    private Map<Double, Double> polarUpAngle;
-    private Map<Double, Double> polarDownAngle;
-    private Map<Double, Double> polarUpSpeed;
-    private Map<Double, Double> polarDownSpeed;
+    private double windSpeed;
+    private Map<Double, Double> mapSpeedAtAngles;
+    private double upWindAngle;
+    private double upWindSpeed;
+    private double downWindAngle;
+    private double downWindSpeed;
 
+    /**
+     * Constructor for Polar.
+     * Note: Adds Upwind and down wind angles to hashmap also.
+     * @param windSpeed Speed of the wind
+     * @param upWindAngle Upwind threshold angle
+     * @param upWindSpeed Upwind speed
+     * @param downWindAngle Downwind threshold angle
+     * @param downWindSpeed Downwind speed
+     */
+    public Polar(double windSpeed, double upWindAngle, double upWindSpeed, double downWindAngle, double downWindSpeed){
+        this.windSpeed = windSpeed;
+        this.upWindAngle = upWindAngle;
+        this.upWindSpeed = upWindSpeed;
+        this.downWindAngle = downWindAngle;
+        this.downWindSpeed = downWindSpeed;
+        mapSpeedAtAngles = new HashMap<>();
+        addToMap(this.upWindAngle, this.upWindSpeed);
+        addToMap(this.downWindAngle, this.downWindSpeed);
+    }
 
-    public Polar(Map<Double, Double> polarUpAngle,
-                 Map<Double, Double> polarDownAngle,
-                 Map<Double, Double> polarUpSpeed,
-                 Map<Double, Double> polarDownSpeed) {
-        this.polarUpAngle = polarUpAngle;
-        this.polarDownAngle = polarDownAngle;
-        this.polarUpSpeed = polarUpSpeed;
-        this.polarDownSpeed = polarDownSpeed;
+    /**
+     * Method to add a value and key to the hashmap of angles and speeds.
+     * @param trueWindAngle True wind angle for boat
+     * @param boatSpeed Speed boat travel at the angle
+     */
+    public void addToMap(double trueWindAngle, double boatSpeed){
+        mapSpeedAtAngles.put(trueWindAngle,boatSpeed);
+    }
+
+    public double getWindSpeed() {
+        return windSpeed;
+    }
+
+    public Map<Double, Double> getMapSpeedAtAngles() {
+        return mapSpeedAtAngles;
+    }
+
+    public double getUpWindAngle() {
+        return upWindAngle;
+    }
+
+    public double getUpWindSpeed() {
+        return upWindSpeed;
+    }
+
+    public double getDownWindAngle() {
+        return downWindAngle;
+    }
+
+    public double getDownWindSpeed() {
+        return downWindSpeed;
     }
 }
