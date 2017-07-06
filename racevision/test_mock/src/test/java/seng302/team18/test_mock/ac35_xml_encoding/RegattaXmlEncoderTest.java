0ac0c2d623110f1680e0145e8878fbc9850bd426
hqi19 <hqi19@uclive.ac.nz>
2017-07-06 17:05:09 +1200

    Change test for RegattaXmlEncoder.
    - Change setUp() method so the tests can use the correct information to test encoder.
    
    #story[1058]

--- racevision/test_mock/src/test/java/seng302/team18/test_mock/ac35_xml_encoding/RegattaXmlEncoderTest.java
+++ racevision/test_mock/src/test/java/seng302/team18/test_mock/ac35_xml_encoding/RegattaXmlEncoderTest.java
@@ -23,10 +23,10 @@ public class RegattaXmlEncoderTest {
 
 
     /**
-     * Create a new AC35 regatta message.
+     * Create a new AC35 regatta message and pass it to the encoder.
      */
     @Before
-    public void setUp() {
+    public void setUp() throws ParserConfigurationException, TransformerException{
         double centralLat = -36.8279;
         double centralLong = 174.812;
         String utcOffset = "12";
@@ -34,29 +34,30 @@ public class RegattaXmlEncoderTest {
         String name = "New Zealand Test";
 
         regattaMessage = new AC35XMLRegattaMessage(id, name, centralLat, centralLong, utcOffset);
-    }
 
-
-    @Test
-    public void RegattaXmlEncoderTest() throws ParserConfigurationException, TransformerException {
         DOMSource domSource = regattaEncoder.getDomSource(regattaMessage);
         Document doc = (Document) domSource.getNode();
         doc.getDocumentElement().normalize();
 
-//        final String REGATTA_TAG = "RegattaConfig";
-
         root = (Element) doc.getElementsByTagName(AC35XmlRegattaComponents.ROOT_REGATTA.toString()).item(0);
     }
 
+
+    /**
+     * Test on encoding id.
+     */
     @Test
     public void encodeIdTest() {
-        System.out.println(root.getChildNodes());
         int encodedId = Integer.parseInt(root.getElementsByTagName(AC35XmlRegattaComponents.ELEMENT_REGATTA_ID.toString())
                 .item(0).getTextContent());
         int id = regattaMessage.getId();
         assertEquals(id, encodedId);
     }
 
+
+    /**
+     * Test on encoding name.
+     */
     @Test
     public void encodeNameTest() {
         String encodedName = root.getElementsByTagName(AC35XmlRegattaComponents.ELEMENT_REGATTA_NAME.toString())
@@ -65,23 +66,33 @@ public class RegattaXmlEncoderTest {
         assertEquals(name, encodedName);
     }
 
+
+    /**
+     * Test on encoding center latitude.
+     */
     @Test
     public void encodeCenterLatTest() {
         double encodedLat = Double.parseDouble(root.getElementsByTagName(AC35XmlRegattaComponents.ELEMENT_REGATTA_CENTER_LAT.toString())
                 .item(0).getTextContent());
         double lat = regattaMessage.getCentralLat();
-        assertEquals(lat, encodedLat);
+        assertEquals(lat, encodedLat, 0.1);
     }
 
+    /**
+     * Test on encoding center longitude.
+     */
     @Test
     public void encodeCenterLongTest() {
         double encodedLon = Double.parseDouble(root.getElementsByTagName(AC35XmlRegattaComponents.ELEMENT_REGATTA_CENTER_LONG.toString())
                 .item(0).getTextContent());
         double lon = regattaMessage.getCentralLong();
-        assertEquals(lon, encodedLon);
+        assertEquals(lon, encodedLon, 0.1);
     }
 
 
+    /**
+     * Test on encoding UTC offset.
+     */
     @Test
     public void encodeUtcOffsetTest() {
         String encodedOffset = root.getElementsByTagName(AC35XmlRegattaComponents.ELEMENT_REGATTA_OFFSET.toString())
