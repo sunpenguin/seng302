ab056f244041f7a02c8c7918cbd51fe609a66222
Anton Jelinek <afj19@uclive.ac.nz>
2017-07-18 12:28:27 +1200

    Document and style IRaceXmlDefaults
    #story[1058]

--- racevision/test_mock/src/main/java/seng302/team18/test_mock/model/IRaceXmlDefaults.java
+++ racevision/test_mock/src/main/java/seng302/team18/test_mock/model/IRaceXmlDefaults.java
@@ -3,7 +3,11 @@ package seng302.team18.test_mock.model;
 import seng302.team18.message.AC35XMLRaceMessage;
 
 /**
- * Created by afj19 on 10/07/17.
+ * This interface provides a skeleton for classes that supply the default values used when building a Race.xml message
+ * from model classes.
+ * <p>
+ * When the model supports all the data currently given by defaults, this interface and its implementations will no
+ * longer be necessary
  */
 public interface IRaceXmlDefaults {
 
