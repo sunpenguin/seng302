680bda1699d8004795c8fba6c5d2d6b569e43898
Anton Jelinek <afj19@uclive.ac.nz>
2017-07-20 11:19:05 +1200

    Document and style RaceXmlResults
    #story[1058]

--- racevision/test_mock/src/main/java/seng302/team18/test_mock/model/RaceXmlDefaults.java
+++ racevision/test_mock/src/main/java/seng302/team18/test_mock/model/RaceXmlDefaults.java
@@ -3,7 +3,7 @@ package seng302.team18.test_mock.model;
 import seng302.team18.message.AC35XMLRaceMessage;
 
 /**
- * Created by afj19 on 10/07/17.
+ * Defaults for when building a Race.xml message from model classes
  */
 public class RaceXmlDefaults implements IRaceXmlDefaults {
 
