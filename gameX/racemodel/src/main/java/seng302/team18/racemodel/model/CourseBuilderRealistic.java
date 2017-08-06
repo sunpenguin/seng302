9e9274913c33c770587e9546624a8cb4a5944cbe
Anton Jelinek <afj19@uclive.ac.nz>
2017-08-06 18:21:28 +1200

    Adjust the marks to make the course an appropriate size
    #story[1143]

--- gameX/racemodel/src/main/java/seng302/team18/racemodel/model/CourseBuilderRealistic.java
+++ gameX/racemodel/src/main/java/seng302/team18/racemodel/model/CourseBuilderRealistic.java
@@ -30,13 +30,13 @@ public class CourseBuilderRealistic extends AbstractCourseBuilder {
     @Override
     protected List<CompoundMark> buildCompoundMarks() {
         // Start Line
-        Mark start1 = new Mark(231, new Coordinate(51.577251, -3.987752));
+        Mark start1 = new Mark(231, new Coordinate(51.57824, -3.98908));
         start1.setHullNumber("LC21");
         start1.setStoweName("PRO");
         start1.setShortName("S1");
         start1.setBoatName("Start Line 1");
 
-        Mark start2 = new Mark(232, new Coordinate(51.576266, -3.985881));
+        Mark start2 = new Mark(232, new Coordinate(51.57746, -3.98828));
         start2.setHullNumber("LC22");
         start2.setStoweName("PIN");
         start2.setShortName("S2");
@@ -45,7 +45,7 @@ public class CourseBuilderRealistic extends AbstractCourseBuilder {
         CompoundMark startGate = new CompoundMark("Start Line", Arrays.asList(start1, start2), 11);
 
         // Mark 1
-        Mark mark1 = new Mark(233, new Coordinate(51.581681, -3.980099));
+        Mark mark1 = new Mark(233, new Coordinate(51.57905, -3.98466));
         mark1.setHullNumber("LC23");
         mark1.setStoweName("M1");
         mark1.setShortName("M1");
@@ -54,13 +54,13 @@ public class CourseBuilderRealistic extends AbstractCourseBuilder {
         CompoundMark compoundMark1 = new CompoundMark("Mark 1", Collections.singletonList(mark1), 12);
 
         // Windward Mark
-        Mark wind1 = new Mark(234, new Coordinate(51.585468, -3.987611));
+        Mark wind1 = new Mark(234, new Coordinate(51.58072, -3.98796));
         wind1.setHullNumber("LC24");
         wind1.setStoweName("PRO");
         wind1.setShortName("WG1");
         wind1.setBoatName("Windward Gate 1");
 
-        Mark wind2 = new Mark(235, new Coordinate(51.586557, -3.986318));
+        Mark wind2 = new Mark(235, new Coordinate(51.58121, -3.98672));
         wind2.setHullNumber("LC25");
         wind2.setStoweName("PIN");
         wind2.setShortName("WG2");
@@ -69,13 +69,13 @@ public class CourseBuilderRealistic extends AbstractCourseBuilder {
         CompoundMark windwardGate = new CompoundMark("Windward Gate", Arrays.asList(wind1, wind2), 13);
 
         // Leeward Mark
-        Mark lee1 = new Mark(236, new Coordinate(51.577927, -3.974071));
+        Mark lee1 = new Mark(236, new Coordinate(51.57722, -3.98241));
         lee1.setHullNumber("LC26");
         lee1.setStoweName("PRO");
         lee1.setShortName("LG1");
         lee1.setBoatName("Leeward Gate 1");
 
-        Mark lee2 = new Mark(237, new Coordinate(51.578840, -3.972075));
+        Mark lee2 = new Mark(237, new Coordinate(51.57779, -3.98122));
         lee2.setHullNumber("LC27");
         lee2.setStoweName("PIN");
         lee2.setShortName("LG2");
@@ -84,13 +84,13 @@ public class CourseBuilderRealistic extends AbstractCourseBuilder {
         CompoundMark leewardGate = new CompoundMark("Leeward Gate", Arrays.asList(lee1, lee2), 14);
 
         // Finish Line
-        Mark finish1 = new Mark(238, new Coordinate(51.589288, -3.982857));
+        Mark finish1 = new Mark(238, new Coordinate(51.58232, -3.98359));
         finish1.setHullNumber("LC28");
         finish1.setStoweName("PRO");
         finish1.setShortName("F1");
         finish1.setBoatName("Finish Line 1");
 
-        Mark finish2 = new Mark(239, new Coordinate(51.588255, -3.982149));
+        Mark finish2 = new Mark(239, new Coordinate(51.58168, -3.98356));
         finish2.setHullNumber("LC29");
         finish2.setStoweName("PIN");
         finish2.setShortName("F2");
