5f97bf90c81900a2177b771e5b0d5d699e7c6759
Anton Jelinek <afj19@uclive.ac.nz>
2017-08-16 18:57:51 +1200

    Make boats start with heading towards first mark
    #story[1154]

--- gameX/common/src/main/java/seng302/team18/model/Race.java
+++ gameX/common/src/main/java/seng302/team18/model/Race.java
@@ -98,7 +98,7 @@ public class Race {
             boat.setCoordinate(getStartPosition(boat, boat.getLength() * 3));
             boat.setHeading(gps.getBearing(
                     course.getMarkSequence().get(0).getCompoundMark().getCoordinate(),
-                    course.getMarkSequence().get(0).getCompoundMark().getCoordinate()
+                    course.getMarkSequence().get(1).getCompoundMark().getCoordinate()
             ));
             boat.setSpeed(boat.getBoatTWS(course.getWindSpeed(), course.getWindDirection()));
             boat.setRoundZone(Boat.RoundZone.ZONE1);
