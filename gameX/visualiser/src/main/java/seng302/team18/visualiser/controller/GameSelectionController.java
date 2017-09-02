35f215065dda8f7ecea56cdcf48923b6adacb58d
Callum Slaven <csl62@uclive.ac.nz>
2017-09-02 13:46:52 +1200

    Added ability to connect to custom host and port inside the secondary pre race screen - added functionality to the buttons
    #story[1150]

--- gameX/visualiser/src/main/java/seng302/team18/visualiser/controller/GameSelectionController.java
+++ gameX/visualiser/src/main/java/seng302/team18/visualiser/controller/GameSelectionController.java
@@ -1,17 +1,29 @@
 package seng302.team18.visualiser.controller;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import seng302.team18.messageparsing.AC35MessageParserFactory;
+import seng302.team18.messageparsing.Receiver;
+import seng302.team18.model.Race;
+import seng302.team18.model.RaceMode;
+import seng302.team18.send.ControllerMessageFactory;
+import seng302.team18.send.Sender;
+
+import javax.net.SocketFactory;
+import java.net.Socket;
 
 /**
  * Controller For the game type selection screen
  */
 public class GameSelectionController {
 
-    @FXML private Pane pane;
+    @FXML private Pane outerPane;
+    @FXML private Pane innerPane;
     private Stage stage;
 
 
@@ -24,14 +36,38 @@ public class GameSelectionController {
         initialiseArcadeButton();
         initialiseSpyroButton();
         initialiseBumperBoatsButton();
+        registerListeners();
     }
 
+    /**
+     * Register any necessary listeners.
+     */
+    private void registerListeners() {
+        outerPane.widthProperty().addListener((observableValue, oldWidth, newWidth) -> reDraw());
+        outerPane.heightProperty().addListener((observableValue, oldHeight, newHeight) -> reDraw());
+    }
+
+
+    /**
+     * Re draw the the pane which holds elements of the play interface to be in the middle of the window.
+     */
+    public void reDraw() {
+        if (!(innerPane.getWidth() == 0)) {
+            innerPane.setLayoutX((outerPane.getScene().getWidth() / 2) - (innerPane.getWidth() / 2));
+            innerPane.setLayoutY((innerPane.getScene().getHeight() / 2) - (innerPane.getHeight() / 2));
+        }
+
+        else if (stage != null) {
+            innerPane.setLayoutX((stage.getWidth() / 2) - (400));
+            innerPane.setLayoutY((stage.getHeight() / 2) - (400));
+        }
+    }
 
     private void initialiseRaceButton() {
         raceLabel = new Label();
         raceLabel.getStylesheets().add(this.getClass().getResource("/stylesheets/titleScreen.css").toExternalForm());
         raceLabel.getStyleClass().add("raceImage");
-        pane.getChildren().add(raceLabel);
+        innerPane.getChildren().add(raceLabel);
 
         raceButtonImage = new Image("/images/RaceWhite.png");
         raceLabel.setLayoutX((600 / 2) - (Math.floorDiv((int) raceButtonImage.getWidth(), 2)));
@@ -39,23 +75,25 @@ public class GameSelectionController {
         raceLabel.setOnMouseClicked(event -> startBumperBoats());
     }
 
+
     private void initialiseArcadeButton() {
         raceLabel = new Label();
         raceLabel.getStylesheets().add(this.getClass().getResource("/stylesheets/titleScreen.css").toExternalForm());
         raceLabel.getStyleClass().add("arcadeImage");
-        pane.getChildren().add(raceLabel);
+        innerPane.getChildren().add(raceLabel);
 
-        raceButtonImage = new Image("/images/Arcade_race_White.png");
+        raceButtonImage = new Image("/images/Arcade_Race_White.png");
         raceLabel.setLayoutX((600 / 2) - (Math.floorDiv((int) raceButtonImage.getWidth(), 2)));
         raceLabel.setLayoutY((600 / 2) + 50);
         raceLabel.setOnMouseClicked(event -> startBumperBoats());
     }
 
+
     private void initialiseSpyroButton() {
         raceLabel = new Label();
         raceLabel.getStylesheets().add(this.getClass().getResource("/stylesheets/titleScreen.css").toExternalForm());
         raceLabel.getStyleClass().add("spyroImage");
-        pane.getChildren().add(raceLabel);
+        innerPane.getChildren().add(raceLabel);
 
         raceButtonImage = new Image("/images/SpyroWhite.png");
         raceLabel.setLayoutX((600 / 2) - (Math.floorDiv((int) raceButtonImage.getWidth(), 2)));
@@ -67,12 +105,47 @@ public class GameSelectionController {
         raceLabel = new Label();
         raceLabel.getStylesheets().add(this.getClass().getResource("/stylesheets/titleScreen.css").toExternalForm());
         raceLabel.getStyleClass().add("bumperBoatsImage");
-        pane.getChildren().add(raceLabel);
+        innerPane.getChildren().add(raceLabel);
 
         raceButtonImage = new Image("/images/bumper-boatsWhite.png");
-        raceLabel.setLayoutX((pane.getWidth() / 2) - (Math.floorDiv((int) raceButtonImage.getWidth(), 2)));
-        raceLabel.setLayoutY((pane.getHeight() / 2) + 150);
+        raceLabel.setLayoutX((600 / 2) - (Math.floorDiv((int) raceButtonImage.getWidth(), 2)));
+        raceLabel.setLayoutY((600 / 2) + 150);
         raceLabel.setOnMouseClicked(event -> startBumperBoats());
+    }
+
+    private void startConnection(Receiver receiver, Sender sender) throws Exception {
+        Stage stage = (Stage) innerPane.getScene().getWindow();
+        FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("PreRace.fxml"));
+        Parent root = loader.load();
+        PreRaceController controller = loader.getController();
+        controller.setStage(stage);
+        stage.setTitle("High Seas");
+        outerPane.getScene().setRoot(root);
+        stage.show();
+
+        Race race = new Race();
+        race.setMode(mode);
+        controller.setUp(race, receiver, sender);
+        controller.initConnection(boatColours.get(colourIndex));
+    }
+
+    private void openStream(String host, int port) {
+        try {
+            Socket socket = SocketFactory.getDefault().createSocket(host, port);
+            startConnection(new Receiver(socket, new AC35MessageParserFactory()), new Sender(socket, new ControllerMessageFactory()));
+        } catch (Exception e) {
+            System.out.println();        }
+    }
+
+    public void hostRace(){
+                createModel();
+        try {
+            Thread.sleep(400);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        mode = RaceMode.RACE;
+        openStream("127.0.0.1", 5005);
     }
 
     private void startBumperBoats(){
