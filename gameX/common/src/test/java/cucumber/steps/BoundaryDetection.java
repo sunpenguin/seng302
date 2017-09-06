5a4f1c0745833d2454c6ab738dd8fcf8d8ef710d
Sam Beeston <sbe67@uclive.ac.nz>
2017-09-06 16:53:58 +1200

    fixing cucumber
    #story[1204]

--- gameX/common/src/test/java/cucumber/steps/BoundaryDetection.java
+++ gameX/common/src/test/java/cucumber/steps/BoundaryDetection.java
@@ -20,16 +20,6 @@ public class BoundaryDetection {
     private Race race;
     private BoatStatus oldStatus = BoatStatus.PRE_START;
     private BoatStatus newStatus = BoatStatus.PRE_START;
-    private boolean hasChanged = false;
-    private Observer observer = new Observer() {
-        @Override
-        public void update(Observable o, Object arg) {
-            if (arg instanceof Boat) {
-                Boat boat =  (Boat) arg;
-                newStatus = boat.getStatus();
-            }
-        }
-    };
 
     @Given("^a course$")
     public void a_course() throws Throwable {
@@ -68,7 +58,6 @@ public class BoundaryDetection {
     @Then("^the players status will stay the same$")
     public void the_players_status_will_stay_the_same() throws Throwable {
         Assert.assertEquals(oldStatus, boat.getStatus());
-        Assert.assertEquals(oldStatus, newStatus);
     }
 
     @When("^the player moves outside the boundary$")
@@ -81,7 +70,6 @@ public class BoundaryDetection {
     @Then("^the players status will be set to disqualified\\.$")
     public void the_players_status_will_be_set_to_disqualified() throws Throwable {
         Assert.assertEquals(BoatStatus.DSQ, boat.getStatus());
-        Assert.assertEquals(BoatStatus.DSQ, newStatus);
     }
 
 
