8fc35f142ef9e13c145d89da229dbe497cb78ce9
Justin Thorby <jth102@uclive.ac.nz>
2017-09-07 22:11:02 +1200

    Implementing a timer for Bumper Boats mode.
    - Created abstract super class "Clock" for all visual clock types
    - Created subclasse StopWatchClock which retains original functionality of RaceClock
    - Created subsclass TimerClock which is only for counting down in necessary modes (for now only BumperBoats). This clock will stop at 0, rather than start counting up. To set a timer, set a start time in config.txt to how many seconds the timer should last. Admittedly this is a bit confusing, but this is because we use the difference in seconds between current time and start time to set the time on the clock's label (regardless of clock type).
    
    #story[1204]

--- gameX/visualiser/src/main/java/seng302/team18/visualiser/interpret/RaceClockInterpreter.java
+++ gameX/visualiser/src/main/java/seng302/team18/visualiser/interpret/RaceClockInterpreter.java
@@ -3,25 +3,25 @@ package seng302.team18.visualiser.interpret;
 import seng302.team18.interpret.MessageInterpreter;
 import seng302.team18.message.AC35RaceStatusMessage;
 import seng302.team18.message.MessageBody;
-import seng302.team18.visualiser.display.RaceClock;
+import seng302.team18.visualiser.display.Clock;
 
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 
 /**
- * A MessageInterpreter that takes a AC35RaceStatusMessage and updates the time on a RaceClock.
+ * A MessageInterpreter that takes a AC35RaceStatusMessage and updates the time on a StopWatchClock.
  */
 public class RaceClockInterpreter extends MessageInterpreter {
 
-    private RaceClock raceClock;
+    private Clock clock;
 
     /**
-     * Constructor for RaceClockInterpreter. Takes a RaceClock as a parameter which it updates every time a
+     * Constructor for RaceClockInterpreter. Takes a StopWatchClock as a parameter which it updates every time a
      * AC35RaceStatusMessage is interpreted.
-     * @param raceClock to be updated.
+     * @param clock to be updated.
      */
-    public RaceClockInterpreter(RaceClock raceClock) {
-        this.raceClock = raceClock;
+    public RaceClockInterpreter(Clock clock) {
+        this.clock = clock;
     }
 
     @Override
@@ -30,7 +30,7 @@ public class RaceClockInterpreter extends MessageInterpreter {
             AC35RaceStatusMessage statusMessage = (AC35RaceStatusMessage) message;
             Instant startIn = Instant.ofEpochMilli(statusMessage.getStartTime());
             Instant currentIn = Instant.ofEpochMilli(statusMessage.getCurrentTime());
-            raceClock.setTime(ChronoUnit.SECONDS.between(startIn, currentIn));
+            clock.setTime(ChronoUnit.SECONDS.between(startIn, currentIn));
         }
     }
 }
