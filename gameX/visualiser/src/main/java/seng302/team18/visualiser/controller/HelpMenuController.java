8c713698742440ec7950146721dde82814c7fb0d
Justin Thorby <jth102@uclive.ac.nz>
2017-09-21 21:35:53 +1200

    Re-making the tab selectors for help menu.
    -Had to change from labels to toggle buttons as labels don't allow to set an image for when selected.
    -Tabs will get underlined when hovered over or selected by showing a new image.
    -Tabs cannot be un-selected like a standard toggle button by disabling them while on a certain tab.
    
    #story[1283]

--- gameX/visualiser/src/main/java/seng302/team18/visualiser/controller/HelpMenuController.java
+++ gameX/visualiser/src/main/java/seng302/team18/visualiser/controller/HelpMenuController.java
@@ -1,20 +1,41 @@
 package seng302.team18.visualiser.controller;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ToggleButton;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 
 public class HelpMenuController {
 
-    @FXML private Label gameModeLabel;
     @FXML private HBox tabHBox;
-    @FXML private Label controlsLabel;
     @FXML private Pane contentPane;
+    @FXML private ToggleButton controlsToggle;
+    @FXML private ToggleButton modesToggle;
 
 
     @FXML public void initialize() {
-        gameModeLabel.getStyleClass().add("gameModeImage");
-        controlsLabel.getStyleClass().add("controlsImage");
+        controlsToggle.getStyleClass().add("controlsImage");
+        controlsToggle.setOnMouseClicked(event -> showControlsHelp());
+
+        modesToggle.getStyleClass().add("gameModeImage");
+        modesToggle.setOnMouseClicked(event -> showGameModeHelp());
+    }
+
+
+    private void showControlsHelp() {
+        modesToggle.setSelected(false);
+        modesToggle.setDisable(false);
+        controlsToggle.setDisable(true);
+        controlsToggle.setOpacity(1.0);
+    }
+
+
+    private void showGameModeHelp() {
+        controlsToggle.setSelected(false);
+        controlsToggle.setDisable(false);
+        modesToggle.setDisable(true);
+        modesToggle.setOpacity(1.0);
     }
 }
