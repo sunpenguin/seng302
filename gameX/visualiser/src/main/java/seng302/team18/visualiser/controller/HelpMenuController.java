bdfa644dfe785c21739209376e47129fb5abfa13
Justin Thorby <jth102@uclive.ac.nz>
2017-09-21 23:03:01 +1200

    Implementing 2 content panes for help screen: Controls and game modes.
    - Can switch between the two tabs to display either controls help or game mode help
    - Only descriptions need to be written now. Keys have been created.
    
    #story[1283]

--- gameX/visualiser/src/main/java/seng302/team18/visualiser/controller/HelpMenuController.java
+++ gameX/visualiser/src/main/java/seng302/team18/visualiser/controller/HelpMenuController.java
@@ -1,18 +1,21 @@
 package seng302.team18.visualiser.controller;
 
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
+import javafx.scene.Node;
+import javafx.scene.control.Toggle;
 import javafx.scene.control.ToggleButton;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
 
 public class HelpMenuController {
 
     @FXML private HBox tabHBox;
-    @FXML private Pane contentPane;
     @FXML private ToggleButton controlsToggle;
     @FXML private ToggleButton modesToggle;
+    @FXML private Pane controlsPane;
+    @FXML private Pane modePane;
 
 
     @FXML public void initialize() {
@@ -21,21 +24,43 @@ public class HelpMenuController {
 
         modesToggle.getStyleClass().add("gameModeImage");
         modesToggle.setOnMouseClicked(event -> showGameModeHelp());
+
+        showControlsHelp();
     }
 
 
+    /**
+     * Show the keyboard controls help tab
+     */
     private void showControlsHelp() {
-        modesToggle.setSelected(false);
-        modesToggle.setDisable(false);
-        controlsToggle.setDisable(true);
-        controlsToggle.setOpacity(1.0);
+        switchTab(modesToggle, modePane, controlsToggle, controlsPane);
     }
 
 
+    /**
+     * Show the game mode help tab
+     */
     private void showGameModeHelp() {
-        controlsToggle.setSelected(false);
-        controlsToggle.setDisable(false);
-        modesToggle.setDisable(true);
-        modesToggle.setOpacity(1.0);
+        switchTab(controlsToggle, controlsPane, modesToggle, modePane);
+    }
+
+
+    /**
+     * Switch the displayed tab from the previous to the selected tab
+     *
+     * @param unselected ToggleButton that has been unselected
+     * @param previous previous pane
+     * @param selected ToggleButton that has been selected
+     * @param next next pane (pane to be shown)
+     */
+    private void switchTab(ToggleButton unselected, Pane previous, ToggleButton selected, Pane next) {
+        unselected.setSelected(false);
+        unselected.setDisable(false);
+        selected.setSelected(true);
+        selected.setDisable(true);
+        selected.setOpacity(1.0);
+
+        previous.setVisible(false);
+        next.setVisible(true);
     }
 }
