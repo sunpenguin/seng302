df8c3ba8b8bc298e93b3853281b93d3a4080cc8a
Anton Jelinek <afj19@uclive.ac.nz>
2017-09-26 01:34:29 +1300

    - Make rounding arrows use correct rounding direction - Make mark rounding arrows appear for both marks in a gate
    #story[1288]

--- gameX/visualiser/src/main/java/seng302/team18/visualiser/display/DisplayRoundingArrow.java
+++ gameX/visualiser/src/main/java/seng302/team18/visualiser/display/DisplayRoundingArrow.java
@@ -7,18 +7,20 @@ import javafx.scene.shape.ArcType;
 import javafx.scene.shape.Polygon;
 import javafx.scene.shape.Shape;
 import javafx.scene.transform.Rotate;
+import seng302.team18.model.AbstractBoat;
 import seng302.team18.model.MarkRounding;
 import seng302.team18.util.XYPair;
 import seng302.team18.visualiser.util.PixelMapper;
+
+import java.util.ArrayList;
+import java.util.Collection;
 
 /**
  * Class for information for arrow that rounds a mark.
  */
 public class DisplayRoundingArrow {
 
-    private MarkRounding markRounding;
-    private Shape head;
-    private Arc tail;
+    private Collection<Shape> shapes = new ArrayList<>();
     private PixelMapper pixelMapper;
 
     private final static Color COLOR = Color.GREEN;
@@ -26,30 +28,52 @@ public class DisplayRoundingArrow {
 
     public DisplayRoundingArrow(PixelMapper pixelMapper, MarkRounding markRounding) {
         this.pixelMapper = pixelMapper;
-        this.markRounding = markRounding;
 
-        drawArrow();
+        if (markRounding.getCompoundMark().isGate()) {
+            boolean isPS = markRounding.getRoundingDirection().equals(MarkRounding.Direction.PS);
+            drawArrow(markRounding.getCompoundMark().getMarks().get(0), !isPS);
+            drawArrow(markRounding.getCompoundMark().getMarks().get(1), isPS);
+        } else {
+            drawArrow(markRounding.getCompoundMark().getMarks().get(0), markRounding.getRoundingDirection().equals(MarkRounding.Direction.STARBOARD));
+        }
     }
 
 
     /**
      * Create the rounding arrow.
      */
-    private void drawArrow() {
-        tail = makeTail();
-        head = makeHeadShape();
+    private void drawArrow(AbstractBoat mark, boolean isClockwise) {
+        Arc tail = makeTail(mark, isClockwise);
+        Shape head = makeHeadShape(mark);
 
-        head.setLayoutX(calculateHeadX());
-        head.setLayoutY(calculateHeadY());
-        head.getTransforms().add(new Rotate(getHeadingAtFinish(), 0, 0));
+        shapes.add(tail);
+        shapes.add(head);
+
+        head.setLayoutX(calculateHeadX(tail));
+        head.setLayoutY(calculateHeadY(tail));
+        head.getTransforms().add(new Rotate(getHeadingAtFinish(tail), 0, 0));
+    }
+
+
+    private Arc makeTail(AbstractBoat mark, boolean isClockwise) {
+        XYPair center = pixelMapper.mapToPane(mark.getCoordinate());
+        final double radius = mark.getLength() * 2 * pixelMapper.mappingRatio();
+        double length = 135 * (isClockwise ? -1 : 1);
+
+        Arc tail = new Arc(center.getX(), center.getY(), radius, radius, 90, length);
+        tail.setType(ArcType.OPEN);
+        tail.setStroke(COLOR);
+        tail.setStrokeWidth(2);
+        tail.setFill(Color.TRANSPARENT);
+        return tail;
     }
 
 
     /**
      * Create the size (shape) of the arrow head.
      */
-    private Shape makeHeadShape() {
-        double pixelLength = markRounding.getCompoundMark().getMarks().get(0).getLength() * pixelMapper.mappingRatio() * 0.2;
+    private Shape makeHeadShape(AbstractBoat mark) {
+        double pixelLength = mark.getLength() * pixelMapper.mappingRatio() * 0.2;
 
         Double[] headShape = new Double[]{
                 0.0, 0.0,
@@ -66,27 +90,13 @@ public class DisplayRoundingArrow {
     }
 
 
-    private Arc makeTail() {
-        XYPair center = pixelMapper.mapToPane(markRounding.getCompoundMark().getMarks().get(0).getCoordinate());
-        final double radius = markRounding.getCompoundMark().getMarks().get(0).getLength() * 2 * pixelMapper.mappingRatio();
-
-        Arc tail = new Arc(center.getX(), center.getY(), radius, radius, 90, 135);
-        tail.setType(ArcType.OPEN);
-        tail.setStroke(COLOR);
-        tail.setStrokeWidth(2);
-        tail.setFill(Color.TRANSPARENT);
-        return tail;
-    }
-
-
     /**
      * Add created arrow to the group.
      *
      * @param group arrow to be added on
      */
     public void addToGroup(Group group) {
-        group.getChildren().add(head);
-        group.getChildren().add(tail);
+        shapes.forEach(shape -> group.getChildren().add(shape));
     }
 
 
@@ -96,22 +106,21 @@ public class DisplayRoundingArrow {
      * @param group arrow to be removed from
      */
     public void removeFromGroup(Group group) {
-        group.getChildren().remove(head);
-        group.getChildren().remove(tail);
+        shapes.forEach(shape -> group.getChildren().remove(shape));
     }
 
 
-    private double calculateHeadX() {
+    private double calculateHeadX(Arc tail) {
         return tail.getCenterX() + tail.getRadiusX() * Math.cos(Math.toRadians(tail.getStartAngle() + tail.getLength()));
     }
 
 
-    private double calculateHeadY() {
+    private double calculateHeadY(Arc tail) {
         return tail.getCenterY() - tail.getRadiusY() * Math.sin(Math.toRadians(tail.getStartAngle() + tail.getLength()));
     }
 
 
-    private double getHeadingAtFinish() {
-        return (tail.getStartAngle() + tail.getLength() + ((tail.getLength() < 0) ? 90 : -90) + 360) % 360;
+    private double getHeadingAtFinish(Arc tail) {
+        return (tail.getStartAngle() + tail.getLength() + 270) % 360;
     }
 }
