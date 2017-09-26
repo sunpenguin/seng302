fa0966e4546086cde8b7183549ff1f0abfc73994
cslaven <csl62@uclive.ac.nz>
2017-09-26 22:43:27 +1300

    adding the aesthetic of the usual game to the pre race screen
    story[1285]

--- gameX/visualiser/src/main/java/seng302/team18/visualiser/controller/PreRaceController.java
+++ gameX/visualiser/src/main/java/seng302/team18/visualiser/controller/PreRaceController.java
@@ -71,6 +71,7 @@ public class PreRaceController {
     public void setUp(ClientRace race, Sender sender, Interpreter interpreter) {
         this.sender = sender;
         this.race = race;
+        System.out.println(race.getRegatta().getName());
         raceNameText.setText(race.getRegatta().getName());
         displayTimeZone(race.getStartTime());
 
