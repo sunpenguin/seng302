11058c043287319496aae426ac4c05c89cf6e8c3
David Lee <dhl25@uclive.ac.nz>
2017-09-27 15:49:16 +1300

    Add documentation for methods in CheatyControlManipulator.
    #story[1286]

--- gameX/visualiser/src/main/java/seng302/team18/visualiser/util/CheatyControlManipulator.java
+++ gameX/visualiser/src/main/java/seng302/team18/visualiser/util/CheatyControlManipulator.java
@@ -16,6 +16,13 @@ public class CheatyControlManipulator {
     public CheatyControlManipulator() {}
 
 
+    /**
+     * Generates the sequence of messages required to turn clockwise.
+     * 
+     * @param windDirection direction of the wind.
+     * @param boatHeading heading of the boat.
+     * @return list of messages to send which will make the boat turn clock wise
+     */
     public List<MessageBody> generateClockwise(double windDirection, double boatHeading) {
         double heading = (boatHeading - windDirection + 360) % 360;
         double flippedHeading = ((boatHeading - windDirection + 360 + 180) % 360);
@@ -49,6 +56,13 @@ public class CheatyControlManipulator {
     }
 
 
+    /**
+     * Generates the sequence of messages required to turn counter clock wise.
+     *
+     * @param windDirection direction of the wind.
+     * @param boatHeading heading of the boat.
+     * @return list of messages to send which will make the boat turn counter clock wise
+     */
     public List<MessageBody> generateCounterClockwise(double windDirection, double boatHeading) {
         double heading = (boatHeading - windDirection + 360) % 360;
         double flippedHeading = ((boatHeading - windDirection + 360 + 180) % 360);
@@ -82,6 +96,13 @@ public class CheatyControlManipulator {
     }
 
 
+    /**
+     * Generates the sequence of messages required to turn upwind.
+     *
+     * @param windDirection direction of the wind.
+     * @param boatHeading heading of the boat.
+     * @return list of messages to send which will make the boat turn up wind
+     */
     public List<MessageBody> generateUpwind(double windDirection, double boatHeading) {
         double heading = (boatHeading - windDirection + 360) % 360;
         fromLeft = !(heading > 180 && heading < 360);
@@ -91,6 +112,13 @@ public class CheatyControlManipulator {
     }
 
 
+    /**
+     * Generates the sequence of messages required to turn downwind.
+     *
+     * @param windDirection direction of the wind.
+     * @param boatHeading heading of the boat.
+     * @return list of messages to send which will make the boat turn downwind
+     */
     public List<MessageBody> generateDownwind(double windDirection, double boatHeading) {
         double heading = (boatHeading - windDirection + 360) % 360;
         fromLeft = !(heading > 0 && heading < 180);
@@ -100,17 +128,30 @@ public class CheatyControlManipulator {
     }
 
 
+    /**
+     * Player id is used for generating messages with the correct id.
+     * @param playerId
+     */
     public void setPlayerId(int playerId) {
         this.playerId = playerId;
     }
 
 
+    /**
+     * Creates an upwind message.
+     * @return an upwind message.
+     */
     private MessageBody makeUpwindMessage() {
         BoatActionMessage actionMessage = new BoatActionMessage(playerId);
         actionMessage.setUpwind();
         return actionMessage;
     }
 
+
+    /**
+     * Creates an downwind message.
+     * @return an downwind message.
+     */
     private MessageBody makeDownwindMessage() {
         BoatActionMessage actionMessage = new BoatActionMessage(playerId);
         actionMessage.setDownwind();
